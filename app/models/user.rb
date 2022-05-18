faa3a86e8345abe491a8d0e5dd29bbfeec6ce6a4
kinyasunori <oneforalltk0826@gmail.com>
2022-05-18 23:11:13 +0900

    Deviseを導入とバリデーション記述

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         validates :nickname,       presence: true
+         validates :family_name,    presence: true
+         validates :first_name,     presence: true
+         validates :read_family,    presence: true
+         validates :read_first,     presence: true
+         validates :birth,          presence: true
+
+         has_many :items
+         has_many :item_purchases
+end
