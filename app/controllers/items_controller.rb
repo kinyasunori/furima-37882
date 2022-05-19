a9a5579f08d6bff79a6023901e95d80fafe5fd16
kinyasunori <oneforalltk0826@gmail.com>
2022-05-19 12:37:31 +0900

    新規登録機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,37 @@
 class ItemsController < ApplicationController
+  
+
   def index
-    
+    @items = Item.all.includes(:user)
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+      @item = Item.includes(:user)
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+  end
+
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:nickname, :family_name, :first_name, :read_family, :read_first, :birth).merge(user_id: current_user.id)
+  end
+
+  
 end
