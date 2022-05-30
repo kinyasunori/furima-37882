9bfb837c4b3915d8c3b7f6583a54e00e0fdcabf6
kinyasunori <oneforalltk0826@gmail.com>
2022-05-30 17:08:04 +0900

    売却済み商品の商品購入ページへの遷移修正

--- app/controllers/item_purchases_controller.rb
+++ app/controllers/item_purchases_controller.rb
@@ -1,14 +1,13 @@
 class ItemPurchasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :move_to_index, only: [:index, :create]
   
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
       pay_item
@@ -32,5 +31,10 @@ class ItemPurchasesController < ApplicationController
         card: purchase_params[:token],    # カードトークン
         currency: 'jpy'                   # 通貨の種類（日本円）
       )
+  end
+
+  def move_to_index
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.item_purchase.present?
   end
 end
